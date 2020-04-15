74cd7bcde7b769d6ea9d0ccc5ba7d5b038c12f5f
Dasha <49146036+shpakovad@users.noreply.github.com>
2020-04-15 14:45:59 +0300

    Updates

---
+++ precache-manifest.cd89647531d93f7729ba5bcbcff79389.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0d8fefced3a77fc430bda471f7f0c01",
+    "url": "/ReactProject/index.html"
+  },
+  {
+    "revision": "d740473139c858b350d7",
+    "url": "/ReactProject/static/css/main.99873945.chunk.css"
+  },
+  {
+    "revision": "966c37e2c64a4617161a",
+    "url": "/ReactProject/static/js/2.daa44cf7.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/ReactProject/static/js/2.daa44cf7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d740473139c858b350d7",
+    "url": "/ReactProject/static/js/main.22a3616c.chunk.js"
+  },
+  {
+    "revision": "508ea2a50a1cf06e6b29",
+    "url": "/ReactProject/static/js/runtime-main.9ee72d4d.js"
+  },
+  {
+    "revision": "7e902781ba761f7b497110d9e64fe908",
+    "url": "/ReactProject/static/media/newsWrapper.7e902781.jpg"
+  },
+  {
+    "revision": "8d5a2dd6586ef4f9a4058a953ffcb840",
+    "url": "/ReactProject/static/media/nyt_logo.8d5a2dd6.jpg"
+  },
+  {
+    "revision": "39270f03b361c985f929073040eee451",
+    "url": "/ReactProject/static/media/wrapperWeather.39270f03.jpg"
+  }
+]);
