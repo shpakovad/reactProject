310e988670c5a9def2a257f39e9517f4c553ada9
Dasha <49146036+shpakovad@users.noreply.github.com>
2020-04-25 23:58:11 +0300

    added sorted popular videos

--- src/Components/Videos/Videos.js
+++ src/Components/Videos/Videos.js
@@ -7,6 +7,23 @@ import {connect} from "react-redux";
 
 class Videos extends PureComponent {
 
+    state={
+        sortType: 'asc',
+        selectedValue: 'All',
+        key: '',
+        track: "",
+        filterValue: "All",
+    };
+    onSort = (sortType, key) => {
+        this.setState({
+            sortType,
+            key
+        })
+    };
+    // onSort=()=>{
+    //     alert('bzn')
+    // }
+
     componentDidMount() {
         this.props.getPopularVideosList(this.props.popularVideos)
     }
@@ -15,8 +32,35 @@ class Videos extends PureComponent {
 
         const {popularVideos} = this.props;
 
+        let sorted = popularVideos.sort((a, b) => {
+            let isReversed = (this.state.sortType === 'asc') ? 1 : -1;
+            switch (this.state.key) {
+                case 'views':
+                    return isReversed * a.props.views.localeCompare(b.props.views);
+                case 'like':
+                    return isReversed * a.videoItem.props.like.localeCompare(b.videoItem.props.like);
+                case 'dislike':
+                    return isReversed * a.props.dislike.localeCompare(b.props.dislike);
+            }
+        });
+
         return (
             <div className="wrapper-video">
+                <form className="sortForm" onSubmit={sorted}>
+                    <div className="form-row align-items-center">
+                        <div className="col-auto my-1">
+                            <select className="custom-select mr-sm-2" id="inlineFormCustomSelect" >
+                                <option selected>Sort by...</option>
+                                <option value="1">Date</option>
+                                <option onClick={()=>{this.onSort('desc','views')}} >Views</option>
+                                <option value="3">Like</option>
+                                <option value="3">Dislike</option>
+                            </select>
+                        </div>
+                    </div>
+                </form>
+
+
                 <NavLink to="/videos_found" className="btn btn-outline-secondary search-btn" type="button"
                          id="button-addon2"> Search Videos </NavLink>
                 <div className="text-popular"> Popular:</div>
@@ -33,14 +77,16 @@ class Videos extends PureComponent {
                                                dislike={videoItem.props.dislike}/>
                         })}
                     </div>
-                </div>
 
+
+            </div>
             </div>
         );
     }
 }
 
 const VideosItem = (props) => {
+
     return (
         <li className="title-popular-video">
             <div className="wrapper-title-video"><span> {props.title} </span></div>
